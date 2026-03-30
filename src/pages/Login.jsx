7602c2b75faed45c5b52a9e9e8b57c7cb56a102f
ixxveon <igayeon322s@gmail.com>
2026-03-30 17:38:56 +0900

    chore(branch): 로컬 브랜치 이동

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -77,7 +77,7 @@ function Login() {
                 localStorage.setItem("loginUser", JSON.stringify(loginUser));
 
                 setUser(loginUser);
-                navigate("/");
+                navigate("/"); 
             },
             onError: (error) => {
                 const message = error?.response?.data?.message || "사번 또는 비밀번호를 확인해주세요.";
