bf5e7676eccf62ff70e822bec08f807b35b951fc
Bora <sbr7518@gmail.com>
2026-03-31 15:45:55 +0900

    수정중

--- src/api/departmentApi.js
+++ src/api/departmentApi.js
@@ -3,7 +3,7 @@ import axiosInstance from "./axiosInstance";
 /**
  * 부서 등록 (POST)
  * @param {Object} deptData - 등록할 부서 정보 (ReqDeptDTO)
- * @returns {Promise<number>} - 생성된 부서 ID(deptid) 반환
+ * @returns {Promise<number>} - 생성된 부서 ID(departmentId) 반환
  */
 export const createDepartmentApi = async (deptData) => {
   try {
@@ -27,7 +27,7 @@ export const getDepartmentListApi = async (page = 0, size = 5) => {
       params: {
         page: page,
         size: size,
-        sort: "deptid,desc"  //  최근 등록순 정렬
+        sort: "departmentId,desc"  //  최근 등록순 정렬
       },
     });
     // Spring Data Page 객체 그대로 반환 
@@ -40,11 +40,11 @@ export const getDepartmentListApi = async (page = 0, size = 5) => {
 
 /**
  * 부서 상세 조회 (GET)
- * @param {number} deptid - URL 경로 변수 (PathVariable)로 전달될 부서 ID
+ * @param {number} departmentId - URL 경로 변수 (PathVariable)로 전달될 부서 ID
  */
-export const getDepartmentDetailApi = async (deptid) => {
+export const getDepartmentDetailApi = async (departmentId) => {
   try {
-    const response = await axiosInstance.get(`/api/departments/${deptid}`);
+    const response = await axiosInstance.get(`/api/departments/${departmentId}`);
     return response.data;
   } catch (error) {
     console.error("부서 상세 조회 실패 : ", error);
@@ -54,12 +54,12 @@ export const getDepartmentDetailApi = async (deptid) => {
 
 /**
  * 부서 정보 수정 (PUT)
- * @param {number} deptid - URL 경로 변수 (PathVariable)
+ * @param {number} departmentId - URL 경로 변수 (PathVariable)
  * @param {Object} deptData - 수정할 내용 (ReqDeptDTO)
  */
-export const updateDepartmentApi = async (deptid, deptData) => {
+export const updateDepartmentApi = async (departmentId, deptData) => {
   try {
-    const response = await axiosInstance.put(`/api/departments/${deptid}`, deptData);
+    const response = await axiosInstance.put(`/api/departments/${departmentId}`, deptData);
     return response.data;
   } catch (error) {
     console.error("부서 수정 실패 : ", error);
@@ -69,11 +69,11 @@ export const updateDepartmentApi = async (deptid, deptData) => {
 
 /**
  * 부서 삭제 (DELETE)
- * @param {number} deptid - URL 경로 변수 (PathVariable)
+ * @param {number} departmentId - URL 경로 변수 (PathVariable)
  */
-export const deleteDepartmentApi = async (deptid) => {
+export const deleteDepartmentApi = async (departmentId) => {
   try {
-    await axiosInstance.delete(`/api/departments/${deptid}`);
+    await axiosInstance.delete(`/api/departments/${departmentId}`);
   } catch (error) {
     console.error("부서 삭제 실패:", error);
     throw error;
