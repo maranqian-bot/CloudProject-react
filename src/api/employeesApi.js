aecbd25f1ca73d64c77ee6c2b0548f713ab026dc
piano <gungang1212@gmail.com>
2026-04-03 16:33:36 +0900

    반영

--- src/api/employeesApi.js
+++ src/api/employeesApi.js
@@ -5,13 +5,13 @@ import axiosInstance from "./axiosInstance";
 // 조회하기
 export const getEmployeesApi = async(params) => {
     // 데이터 오면 바로 반환
-    const response = await axiosInstance.get("/api/employee",{params});
+    const response = await axiosInstance.get("/api/employees",{params});
     return response.data; // data -> content 안쪽의 내용... 스프링에서 보낸 page객체가 들어감
 }
 
 // 게시하기     : axios.get() 매개변수로, 1.경로, 2.데이터객체를 보냄.
 export const postEmployeesApi = async(employeeData) => {
-    const response = await axiostInstance.post("/api/employee/post" , employeeData);
+    const response = await axiosInstane.post("/api/employee/post" , employeeData);
     return response.data;   // 게시 경로와, 데이터 객체 보내주기
 }
 
